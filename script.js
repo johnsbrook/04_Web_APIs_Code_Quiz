aa183dc6219116262567757e85b1e94bf8c00b22
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-07-25 20:59:59 -0400

    commited

---
+++ script.js
@@ -0,0 +1,18 @@
+var questionNo1 = "¿Cuantas piernas hay en una mesa?";
+
+var startQuiz = document.querySelector("#startQuiz");
+    startQuiz = startQuiz.addEventListener("click", function() {
+
+        alert("you've clicked a button")
+
+    });
+
+function answers1() {
+
+    var answer1 = "four legs";
+    var answer2 = "five legs";
+    var answer3 = "six legs";
+    var answer4 = "no legs";
+
+}
+
